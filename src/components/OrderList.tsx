9f604e395fcf30f5bd2cc7232a3eecd273f87e15
Victor Klomp <victor@gewis.nl>
2024-12-29 15:50:12 +0100

    fix(orders): Better order styling

--- src/components/OrderList.tsx
+++ src/components/OrderList.tsx
@@ -11,7 +11,7 @@ export default function OrderList({ orders }: Props) {
     <div className="absolute top-0 right-0 z-50 m-4 text-white" style={{ height: '85%', fontSize: '5.5vh' }}>
       {orders.length > 0 && (
         <div>
-          <div className="top-0 right-0 mb-4 bg-opacity-80 bg-gray-400 p-4 rounded-lg text-right">Ready</div>
+          <div className="top-0 right-0 mb-4 bg-opacity-80 bg-gray-400 p-4 rounded-lg text-center">Ready</div>
           <div
             className="grid grid-flow-col-dense gap-4"
             style={{ gridTemplateRows: 'repeat(6, minmax(0, 1fr))' }}
