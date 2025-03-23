538b2710d882a720e3b54617fd5484fabc3a0bfa
Roy Kakkenberg <me@roykakkenberg.nl>
2025-03-23 21:37:24 +0100

    feat(orders): animate orders on mount and unmount

--- src/components/OrderList.tsx
+++ src/components/OrderList.tsx
@@ -1,4 +1,4 @@
-// import { useEffect } from 'react';
+import { AnimatePresence, motion, TargetAndTransition } from 'framer-motion';
 import { Order } from '../api';
 import GlassCard from './GlassCard';
 
@@ -7,25 +7,43 @@ interface Props {
 }
 
 export default function OrderList({ orders }: Props) {
+  const animationInitial: TargetAndTransition = {
+    scale: 0,
+  };
+  const animationIn: TargetAndTransition = {
+    scale: 1,
+    transition: { delay: 0.5, type: 'spring' },
+  };
+  const animationOut: TargetAndTransition = {
+    opacity: 0,
+    transition: { delay: 0.5 },
+  };
+
   return (
     // bg-blue-500
     <div className="absolute top-0 right-0 z-50 m-4 text-white" style={{ height: '85%', fontSize: '5.5vh' }}>
-      {orders.length > 0 && (
-        <div>
-          <GlassCard className="top-0 right-0 mb-4 px-6 py-4 text-center">Ready</GlassCard>
-          <div
-            className="grid grid-flow-col-dense gap-4"
-            style={{ gridTemplateRows: 'repeat(6, minmax(0, 1fr))' }}
-            dir="rtl"
-          >
-            {orders.map((order, index) => (
-              <GlassCard key={index} className="text-center p-4">
-                {order.number}
-              </GlassCard>
+      <div>
+        <AnimatePresence>
+          {orders.length > 0 && (
+            <motion.div initial={animationInitial} animate={animationIn} exit={animationOut}>
+              <GlassCard className="top-0 right-0 mb-4 px-6 py-4 text-center">Ready</GlassCard>
+            </motion.div>
+          )}
+        </AnimatePresence>
+        <ul
+          className="grid grid-flow-col-dense gap-4"
+          style={{ gridTemplateRows: 'repeat(6, minmax(0, 1fr))' }}
+          dir="rtl"
+        >
+          <AnimatePresence>
+            {orders.map((order) => (
+              <motion.li key={order.number} initial={animationInitial} animate={animationIn} exit={animationOut} layout>
+                <GlassCard className="text-center p-4">{order.number}</GlassCard>
+              </motion.li>
             ))}
-          </div>
-        </div>
-      )}
+          </AnimatePresence>
+        </ul>
+      </div>
     </div>
   );
 }
