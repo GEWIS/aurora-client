ac2338d56580bd5313c858982ab77a89f91d323e
Roy Kakkenberg <me@roykakkenberg.nl>
2025-03-23 20:38:24 +0100

    feat(posters): add Aurora watermark to first poster in carousel

--- src/handlers/poster/components/ProgressBar.tsx
+++ src/handlers/poster/components/ProgressBar.tsx
@@ -1,3 +1,4 @@
+import { LogoLine } from '../../../components/aurora-logos/LogoLine';
 import ProgressBarSlider from './ProgressBarSlider';
 import Clock from './Clock';
 
@@ -31,54 +32,61 @@ export default function ProgressBar({
   progressBarColor = '#c40000',
 }: Props) {
   return (
-    <div
-      className="absolute w-full bottom-0 z-50 text-white flex flex-col text-5xl progress-bar-height"
-      id="progress-bar"
-      style={{ backgroundColor: !minimal && !hide ? 'rgba(0, 0, 0, 0.5)' : '' }}
-    >
-      <div className="absolute w-full" style={{ bottom: minimal || hide ? 0 : '' }} id="progress-bar-slider-outer">
-        {seconds !== undefined && posterIndex !== undefined && (
-          <ProgressBarSlider seconds={seconds} posterIndex={posterIndex} color={progressBarColor} />
-        )}
-      </div>
+    <div className="absolute w-full bottom-0 z-50">
+      {posterIndex === 0 && (
+        <div className="mb-4 opacity-50" id="aurora-watermark">
+          <LogoLine variant="left" size="2em" dark />
+        </div>
+      )}
       <div
-        className={`flex-grow flex justify-center items-center h-full ${hide ? 'hidden' : ''}`}
-        id="progress-bar-container"
+        className="relative text-white flex flex-col text-5xl progress-bar-height"
+        id="progress-bar"
+        style={{ backgroundColor: !minimal && !hide ? 'rgba(0, 0, 0, 0.5)' : '' }}
       >
-        <div className="relative py-3" id="progress-bar-logos">
-          <div className="h-full flex flex-row gap-6 items-center">
-            {logo && (
-              <>
-                <svg style={{ filter: 'drop-shadow(3px 5px 2px rgb(0 0 0 /0.4))' }} viewBox="0 0 50 100">
-                  <image x="0" y="0" height="100%" width="100%" xlinkHref={logo} />
-                </svg>
-                {borrelMode && (
-                  <svg
-                    style={{
-                      filter: 'brightness(0) invert(1) drop-shadow(3px 5px 2px rgb(0 0 0 /0.4))',
-                    }}
-                    viewBox="0 0 100 100"
-                    id="progress-bar-logo-borrelmode"
-                  >
-                    <image x="0" y="0" height="100%" width="100%" xlinkHref={'/borrel/sudosos.svg'} />
-                  </svg>
-                )}
-              </>
-            )}
-          </div>
+        <div className="absolute w-full" style={{ bottom: minimal || hide ? 0 : '' }} id="progress-bar-slider-outer">
+          {seconds !== undefined && posterIndex !== undefined && (
+            <ProgressBarSlider seconds={seconds} posterIndex={posterIndex} color={progressBarColor} />
+          )}
         </div>
-        {/* eslint-disable-next-line jsx-a11y/click-events-have-key-events,jsx-a11y/no-static-element-interactions */}
         <div
-          className="flex-grow text-center text-shadow whitespace-nowrap overflow-hidden text-ellipsis"
-          style={{ minHeight: '1em' }}
-          onClick={pausePoster}
-          id="progress-bar-title"
+          className={`flex-grow flex justify-center items-center h-full ${hide ? 'hidden' : ''}`}
+          id="progress-bar-container"
         >
-          {!minimal && title}
-        </div>
-        {/* eslint-disable-next-line jsx-a11y/click-events-have-key-events,jsx-a11y/no-static-element-interactions */}
-        <div className="text-right" onClick={nextPoster} id="progress-bar-clock">
-          <Clock color={clockColor} shouldTick={clockTick} />
+          <div className="relative py-3" id="progress-bar-logos">
+            <div className="h-full flex flex-row gap-6 items-center">
+              {logo && (
+                <>
+                  <svg style={{ filter: 'drop-shadow(3px 5px 2px rgb(0 0 0 /0.4))' }} viewBox="0 0 50 100">
+                    <image x="0" y="0" height="100%" width="100%" xlinkHref={logo} />
+                  </svg>
+                  {borrelMode && (
+                    <svg
+                      style={{
+                        filter: 'brightness(0) invert(1) drop-shadow(3px 5px 2px rgb(0 0 0 /0.4))',
+                      }}
+                      viewBox="0 0 100 100"
+                      id="progress-bar-logo-borrelmode"
+                    >
+                      <image x="0" y="0" height="100%" width="100%" xlinkHref={'/borrel/sudosos.svg'} />
+                    </svg>
+                  )}
+                </>
+              )}
+            </div>
+          </div>
+          {/* eslint-disable-next-line jsx-a11y/click-events-have-key-events,jsx-a11y/no-static-element-interactions */}
+          <div
+            className="flex-grow text-center text-shadow whitespace-nowrap overflow-hidden text-ellipsis"
+            style={{ minHeight: '1em' }}
+            onClick={pausePoster}
+            id="progress-bar-title"
+          >
+            {!minimal && title}
+          </div>
+          {/* eslint-disable-next-line jsx-a11y/click-events-have-key-events,jsx-a11y/no-static-element-interactions */}
+          <div className="text-right" onClick={nextPoster} id="progress-bar-clock">
+            <Clock color={clockColor} shouldTick={clockTick} />
+          </div>
         </div>
       </div>
     </div>
