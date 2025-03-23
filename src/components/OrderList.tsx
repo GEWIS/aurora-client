cd9086c864d823b76d50dfa1e5a5fa05fc614e22
Roy Kakkenberg <38660000+Yoronex@users.noreply.github.com>
2025-03-23 22:00:30 +0100

    feat(orders): improved order card styling, including new animations (#42)
    * feat(orders): improve the orders overlay so tiles are more glass-like
    
    * feat(orders): animate orders on mount and unmount
    
    * fix(orders): make layout screen-size agnostic
    
    * fix(orders): card width on non-16:9 screens

--- src/components/OrderList.tsx
+++ src/components/OrderList.tsx
@@ -1,30 +1,49 @@
-// import { useEffect } from 'react';
+import { AnimatePresence, motion, TargetAndTransition } from 'framer-motion';
 import { Order } from '../api';
+import GlassCard from './GlassCard';
 
 interface Props {
   orders: Order[];
 }
 
 export default function OrderList({ orders }: Props) {
+  const animationInitial: TargetAndTransition = {
+    scale: 0,
+  };
+  const animationIn: TargetAndTransition = {
+    scale: 1,
+    transition: { delay: 0.5, type: 'spring' },
+  };
+  const animationOut: TargetAndTransition = {
+    opacity: 0,
+    transition: { delay: 0.5 },
+  };
+
   return (
     // bg-blue-500
-    <div className="absolute top-0 right-0 z-50 m-4 text-white" style={{ height: '85%', fontSize: '5.5vh' }}>
-      {orders.length > 0 && (
-        <div>
-          <div className="top-0 right-0 mb-4 bg-opacity-80 bg-gray-400 p-4 rounded-lg text-center">Ready</div>
-          <div
-            className="grid grid-flow-col-dense gap-4"
-            style={{ gridTemplateRows: 'repeat(6, minmax(0, 1fr))' }}
-            dir="rtl"
-          >
-            {orders.map((order, index) => (
-              <div key={index} className="bg-gray-400 p-4 bg-opacity-80 rounded-lg text-center animate-slide-in">
-                {order.number}
-              </div>
+    <div className="absolute top-0 right-0 z-50 m-[1.5vh] text-white" style={{ height: '85%', fontSize: '5.5vh' }}>
+      <div>
+        <AnimatePresence>
+          {orders.length > 0 && (
+            <motion.div initial={animationInitial} animate={animationIn} exit={animationOut}>
+              <GlassCard className="top-0 right-0 mb-[1.5vh] px-[2vh] py-[1.5vh] text-center">Ready</GlassCard>
+            </motion.div>
+          )}
+        </AnimatePresence>
+        <ul
+          className="grid grid-flow-col-dense gap-[1.5vh]"
+          style={{ gridTemplateRows: 'repeat(6, minmax(0, 1fr))' }}
+          dir="rtl"
+        >
+          <AnimatePresence>
+            {orders.map((order) => (
+              <motion.li key={order.number} initial={animationInitial} animate={animationIn} exit={animationOut} layout>
+                <GlassCard className="text-center p-[1.5vh] min-w-[2.3em]">{order.number}</GlassCard>
+              </motion.li>
             ))}
-          </div>
-        </div>
-      )}
+          </AnimatePresence>
+        </ul>
+      </div>
     </div>
   );
 }
