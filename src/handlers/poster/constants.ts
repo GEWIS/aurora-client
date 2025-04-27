fc47c8710bb961a4e4494d7777fa23a7ccb08ffa
Victor Klomp <victor@gewis.nl>
2025-04-27 11:26:45 +0200

    fix(poster): update urls for poster to reflect reality

--- src/handlers/poster/constants.ts
+++ src/handlers/poster/constants.ts
@@ -1,2 +1,2 @@
-export const URL_CUSTOM_STYLESHEET = '/api/handler/screen/gewis-poster/settings/custom-stylesheet';
-export const URL_PROGRESS_BAR_LOGO = '/api/handler/screen/gewis-poster/settings/progress-bar-logo';
+export const URL_CUSTOM_STYLESHEET = '/api/handler/screen/poster/settings/custom-stylesheet';
+export const URL_PROGRESS_BAR_LOGO = '/api/handler/screen/poster/settings/progress-bar-logo';
